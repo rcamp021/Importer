c5043bc02cba713a807d7748feef756142227680
rcamp021 <rcamp021@odu.edu>
2016-12-02 21:24:58 -0500

    Entity Framework can't find meta data

--- Importer/Importer/Program.cs
+++ Importer/Importer/Program.cs
@@ -15,46 +15,59 @@ namespace Importer
             const string Emslocation = @"../../csvs/EMS.csv";
             const string PoliceLocation = @"csvs/Police.csv";
 
-            var emsLines = File.ReadLines(Emslocation).Select(x => x.Split('\n'))
+            var emsLines = File.ReadLines(Emslocation);
+
+            //var arrayLines = from line in emsLines select (line.Split(',')).t
+            var arrayLines = emsLines.Select(x => x.Split(','))
                 .Select(x => new Ems()
                 {
-                    EmsCallNumber =null,// Convert.ToInt32(x.ElementAtOrDefault(0)),
-                    CallPriority =null,// Convert.ToInt32(x.ElementAtOrDefault(1)),
-                    RescueSquadNumber = x.ElementAtOrDefault(2),
-                    CallDateTime = Convert.ToDateTime(x.ElementAtOrDefault(3)),
-                    EntryDateTime = Convert.ToDateTime(x.ElementAtOrDefault(4)),
-                    DispatchDateTime = Convert.ToDateTime(x.ElementAtOrDefault(5)),
-                    EnRouteDateTime = Convert.ToDateTime(x.ElementAtOrDefault(6)),
-                    OnSceneDateTime = Convert.ToDateTime(x.ElementAtOrDefault(7)),
-                    CloseDateTime = Convert.ToDateTime(x.ElementAtOrDefault(8)),
-                    Latitude = Convert.ToDouble(x.ElementAtOrDefault(9)),
-                    Longitude = Convert.ToDouble(x.ElementAtOrDefault(10))
+                    EmsCallNumber = x.ElementAtOrDefault(0).SafeParseInt(),// Convert.ToInt32(x.ElementAtOrDefault(0)),
+                    CallPriority = x.ElementAtOrDefault(1).SafeParseInt(),// Convert.ToInt32(x.ElementAtOrDefault(1)),
+                    RescueSquadNumber = x.ElementAtOrDefault(2)?.Trim(),
+                    CallDateTime = x.ElementAtOrDefault(3).SafeParseDate(),
+                    EntryDateTime = x.ElementAtOrDefault(4).SafeParseDate(),
+                    DispatchDateTime = x.ElementAtOrDefault(5).SafeParseDate(),
+                    EnRouteDateTime = x.ElementAtOrDefault(6).SafeParseDate(),
+                    OnSceneDateTime = x.ElementAtOrDefault(7).SafeParseDate(),
+                    CloseDateTime = x.ElementAtOrDefault(8).SafeParseDate(),
+                    Latitude = Convert.ToDouble(x.ElementAtOrDefault(9).SafeParseDouble()),
+                    Longitude = Convert.ToDouble(x.ElementAtOrDefault(10).SafeParseDouble())
                 });
 
-            insertEms(emsLines);
+            insertEms(arrayLines);
 
+            var t = 0;
             // var lines = File.ReadLines(PoliceLocation).Select(x => x.Split(';'));
 
             //using (var ctx = new cod3Entities())
             //{
-                
+
             //}
 
         }
 
         private static void insertEms(IEnumerable<Ems> emsData)
         {
-            //var counter = 0;
-            //var taking = 1000;
-            //var itemsToInsert = lines.Take(taking);
+            var lines = emsData.ToList();
+            var counter = 0;
+            var taking = 1000;
+            var itemsToInsert = lines.Take(taking);
 
-            //using (var ctx = new cod3Entities())
-            //{
-            //    for (var i = 0; i < lines.Count() / taking; i++)
-            //    {
-            //        ctx.Ems.AddRange(itemsToInsert);
-            //    }
-            //}
+            using (var ctx = new cod3Entities())
+            {
+              //  ctx.Ems.AddRange(itemsToInsert);
+                foreach (var item in itemsToInsert)
+                {
+                    ctx.Ems.Add(item);
+                }
+                ctx.SaveChanges();
+                for (var i = 0; i < lines.Count() / taking; i++)
+                {
+                    ctx.Ems.AddRange(lines.Skip(counter).Take(taking));
+                    counter += taking;
+                    ctx.SaveChanges();
+                }
+            }
         }
     }
 }
---
+++ Importer/Importer/StringExts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Importer
+{
+   public static class StringExts
+    {
+
+        public static DateTime? SafeParseDate(this string that)
+        {
+            DateTime test;
+            return DateTime.TryParse(that, out test) ? test : (DateTime?) null;
+        }
+
+
+        public static double? SafeParseDouble(this string that)
+        {
+            double test;
+            return double.TryParse(that, NumberStyles.Float, CultureInfo.InvariantCulture, out test) ? test : (double?)null;
+        }
+
+       
+        public static int? SafeParseInt(this string that)
+        {
+            int test;
+            return int.TryParse(that, NumberStyles.Integer, CultureInfo.InvariantCulture, out test) ? test : (int?)null;
+        }
+    }
+}
